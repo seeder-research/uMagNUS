40bec9359d0859768c83352958c42cf4884ea5a8
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2021-12-12 21:47:50 +0800

    add uMagNUS64 (untested)

---
+++ cmd/uMagNUS64/main.go
@@ -0,0 +1,189 @@
+// uMagNUS main source
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/seeder-research/uMagNUS/engine64"
+	"github.com/seeder-research/uMagNUS/opencl64"
+	"github.com/seeder-research/uMagNUS/script"
+	"github.com/seeder-research/uMagNUS/util"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"time"
+)
+
+// flags in engine/gofiles.go
+var ()
+
+func main() {
+	flag.Parse()
+	log.SetPrefix("")
+	log.SetFlags(0)
+
+        if *engine.Flag_host {
+                if *engine.Flag_gpu < 0 {
+                        opencl.Init(*engine.Flag_gpu)
+                } else {
+                        log.Fatalln("Cannot disable GPU acceleration while requesting GPU \n")
+                }
+        } else {
+                if *engine.Flag_gpu < 0 {
+                        opencl.Init(0)
+                } else {
+                        opencl.Init(*engine.Flag_gpu)
+                }
+        }
+
+	opencl.Synchronous = *engine.Flag_sync
+	if *engine.Flag_version {
+		printVersion()
+	}
+
+	// used by bootstrap launcher to test opencl
+	// successful exit means opencl was initialized fine
+	if *engine.Flag_test {
+		fmt.Println(opencl.GPUInfo)
+		os.Exit(0)
+	}
+
+	if len(opencl.ClDevices) <= 0 {
+		fmt.Print("No OpenCL devices found \n")
+		os.Exit(0)
+	}
+
+	defer engine.Close() // flushes pending output, if any
+
+	if *engine.Flag_vet {
+		vet()
+		return
+	}
+
+	switch flag.NArg() {
+	case 0:
+		runInteractive()
+	case 1:
+		runFileAndServe(flag.Arg(0))
+	default:
+		RunQueue(flag.Args())
+	}
+}
+
+func runInteractive() {
+	fmt.Println("//no input files: starting interactive session")
+	//initEngine()
+
+	// setup outut dir
+	now := time.Now()
+	outdir := fmt.Sprintf("uMagNUS-%v-%02d-%02d_%02dh%02d.out", now.Year(), int(now.Month()), now.Day(), now.Hour(), now.Minute())
+	engine.InitIO(outdir, outdir, *engine.Flag_forceclean)
+
+	engine.Timeout = 365 * 24 * time.Hour // basically forever
+
+	// set up some sensible start configuration
+	engine.Eval(`SetGridSize(128, 64, 1)
+		SetCellSize(4e-9, 4e-9, 4e-9)
+		Msat = 1e6
+		Aex = 10e-12
+		alpha = 1
+		m = RandomMag()`)
+	addr := goServeGUI()
+	openbrowser("http://127.0.0.1" + addr)
+	engine.RunInteractive()
+}
+
+func runFileAndServe(fname string) {
+	if path.Ext(fname) == ".go" {
+		runGoFile(fname)
+	} else {
+		runScript(fname)
+	}
+}
+
+func runScript(fname string) {
+	outDir := util.NoExt(fname) + ".out"
+	if *engine.Flag_od != "" {
+		outDir = *engine.Flag_od
+	}
+	engine.InitIO(fname, outDir, *engine.Flag_forceclean)
+
+	fname = engine.InputFile
+
+	var code *script.BlockStmt
+	var err2 error
+	if fname != "" {
+		// first we compile the entire file into an executable tree
+		code, err2 = engine.CompileFile(fname)
+		util.FatalErr(err2)
+	}
+
+	// now the parser is not used anymore so it can handle web requests
+	goServeGUI()
+
+	if *engine.Flag_interactive {
+		openbrowser("http://127.0.0.1" + *engine.Flag_port)
+	}
+
+	// start executing the tree, possibly injecting commands from web gui
+	engine.EvalFile(code)
+
+	if *engine.Flag_interactive {
+		engine.RunInteractive()
+	}
+}
+
+func runGoFile(fname string) {
+
+	// pass through flags
+	flags := []string{"run", fname}
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name != "o" {
+			flags = append(flags, fmt.Sprintf("-%v=%v", f.Name, f.Value))
+		}
+	})
+
+	if *engine.Flag_od != "" {
+		flags = append(flags, fmt.Sprintf("-o=%v", *engine.Flag_od))
+	}
+
+	cmd := exec.Command("go", flags...)
+	log.Println("go", flags)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		engine.Close()
+		os.Exit(1)
+	}
+}
+
+// start Gui server and return server address
+func goServeGUI() string {
+	if *engine.Flag_port == "" {
+		log.Println(`//not starting GUI (-http="")`)
+		return ""
+	}
+	addr := engine.GoServe(*engine.Flag_port)
+	fmt.Print("//starting GUI at http://127.0.0.1", addr, "\n")
+	return addr
+}
+
+// print version to stdout
+func printVersion() {
+	engine.LogOut("//", engine.UNAME, "\n")
+	engine.LogOut("//", opencl.GPUInfo, "\n")
+	engine.LogOut("//(c) Xuanyao Fong, SEEDER Research Group", "\n")
+	engine.LogOut("//@ National University of Singapore, Singapore", "\n")
+	engine.LogOut("//Web site: https://blog.nus.edu.sg/seeder", "\n")
+	engine.LogOut("//Email: kelvin.xy.fong@nus.edu.sg", "\n")
+	engine.LogOut("//Source code can be downloaded at https://github.com/seeder-research/uMagNUS", "\n")
+	engine.LogOut("This is free software without any warranty. See license.txt")
+	engine.LogOut("********************************************************************//")
+	engine.LogOut("  If you use uMagNUS in any work or publication,                    //")
+	engine.LogOut("  we kindly ask you to cite the references in references.bib        //")
+	engine.LogOut("********************************************************************//")
+	engine.LogOut("//uMagNUS is an OpenCL-based derivative of MuMax 3.10: (c) Arne Vansteenkiste, Dynamat LAB, Ghent University, Belgium", "\n")
+}
