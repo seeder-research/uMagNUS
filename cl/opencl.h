83270f2e96b1cef376950b3bd9ee22c80269e236
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2022-03-01 15:25:51 +0800

    fix opencl.h to allow for headers up to OpenCL 3.0

--- cl/opencl.h
+++ cl/opencl.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_1_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_2_APIS
+#define CL_TARGET_OPENCL_VERSION 120
 #ifdef __APPLE__
 	#include <OpenCL/OpenCL.h>
 #else
