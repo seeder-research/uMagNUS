2910a4d880a11683cd73dc765ba87918f394872e
Xuanyao Fong <xuanyao.fong@gmail.com>
2022-03-09 10:40:39 +0800

    clean ShowBuildLog()

--- cmd/uMagNUS-clCompiler/program.go
+++ cmd/uMagNUS-clCompiler/program.go
@@ -26,36 +26,40 @@ func ShowBuildLog(p *cl.Program, d *cl.Device) {
 	}
 	switch status {
 	case cl.BuildStatusSuccess:
-		if *Flag_verbose > 3 {
+		if *Flag_verbose > 2 {
 			fmt.Println("  Successfully built program")
 		}
 	case cl.BuildStatusNone:
-		if *Flag_verbose > 3 {
+		if *Flag_verbose > 2 {
 			fmt.Println("  Program was not built/compiled/linked")
 			fmt.Println("    Please run clBuildProgram, clCompileProgram or clLinkProgram")
 		}
 		return
 	case cl.BuildStatusError:
-		if *Flag_verbose > 3 {
+		if *Flag_verbose > 2 {
 			fmt.Println("  Program is built with errors!")
 		}
 	case cl.BuildStatusInProgress:
-		if *Flag_verbose > 3 {
+		if *Flag_verbose > 2 {
 			fmt.Println("  Program build is in progress")
 		}
 		return
 	default:
-		if *Flag_verbose > 3 {
+		if *Flag_verbose > 2 {
 			fmt.Println("  ERROR: Unknown status returned")
 		}
 		return
 	}
 
-	var logout string
-	logout, err = p.GetBuildLog(d)
+	var logOutput string
+	logOutput, err = p.GetBuildLog(d)
 	if err != nil {
 		fmt.Println("  ERROR: unable to get build log of program!")
 		return
 	}
-	fmt.Printf("%+v \n", logout)
+	if logOutput == "" {
+		fmt.Println("Empty log!")
+	} else {
+		fmt.Printf("%+v \n", logOutput)
+	}
 }
