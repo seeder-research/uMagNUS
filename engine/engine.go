e9a7fe663c34d144bba276fbae8d76dd284729d0
Xuanyao Fong <xuanyao.fong@gmail.com>
2022-03-24 16:44:22 +0800

    update version

--- engine/engine.go
+++ engine/engine.go
@@ -20,7 +20,7 @@ import (
 	"time"
 )
 
-const VERSION = "uMagNUS 2.0.1"
+const VERSION = "uMagNUS 2.1.0"
 
 var UNAME = VERSION + " " + runtime.GOOS + "_" + runtime.GOARCH + " " + runtime.Version() + " (" + runtime.Compiler + ")"
 
