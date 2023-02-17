05dede5499faf940261af7d0c73524bd813e7c0d
Xuanyao Fong <xuanyao.fong@gmail.com>
2023-02-17 23:41:04 +0800

    update torque.go

--- opencl/lltorque.go
+++ opencl/lltorque.go
@@ -14,149 +14,114 @@ import (
 //     B in Tesla
 // see lltorque.cl
 func LLTorque(torque, m, B *data.Slice, alpha MSlice) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		lltorque__(torque, m, B, alpha, wg)
+	} else {
+		go lltorque__(torque, m, B, alpha, wg)
+	}
+	wg.Wait()
+}
+
+func lltorque__(torque, m, B *data.Slice, alpha MSlice, wg_ sync.WaitGroup) {
+	torque.Lock(X)
+	torque.Lock(Y)
+	torque.Lock(Z)
+	defer torque.Unlock(X)
+	defer torque.Unlock(Y)
+	defer torque.Unlock(Z)
+	m.RLock(X)
+	m.RLock(Y)
+	m.RLock(Z)
+	defer m.RUnlock(X)
+	defer m.RUnlock(Y)
+	defer m.RUnlock(Z)
+	B.RLock(X)
+	B.RLock(Y)
+	B.RLock(Z)
+	defer B.RUnlock(X)
+	defer B.RUnlock(Y)
+	defer B.RUnlock(Z)
+	alpha.RLock()
+	defer alpha.RUnlock()
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("lltorque failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := torque.Len()
 	cfg := make1DConf(N)
-
-	eventList := []*cl.Event{}
-	tmpEvtL := torque.GetAllEvents(X)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Y)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Z)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvt := m.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	if alpha.GetSlicePtr() != nil {
-		tmpEvt = alpha.GetEvent(0)
-		if tmpEvt != nil {
-			eventList = append(eventList, tmpEvt)
-		}
-	}
-	if len(eventList) == 0 {
-		eventList = nil
-	}
 
 	event := k_lltorque2_async(torque.DevPtr(X), torque.DevPtr(Y), torque.DevPtr(Z),
 		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
 		B.DevPtr(X), B.DevPtr(Y), B.DevPtr(Z),
-		alpha.DevPtr(0), alpha.Mul(0), N, cfg,
-		eventList)
+		alpha.DevPtr(0), alpha.Mul(0), N, cfg, cmdqueue,
+		nil)
 
-	torque.SetEvent(X, event)
-	torque.SetEvent(Y, event)
-	torque.SetEvent(Z, event)
+	wg_.Done()
 
-	glist := []GSlice{m, B}
-	if alpha.GetSlicePtr() != nil {
-		glist = append(glist, alpha)
+	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents failed in lltorque: %+v \n", err)
 	}
-	InsertEventIntoGSlices(event, glist)
-
-	if Debug {
-		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
-			fmt.Printf("WaitForEvents failed in lltorque: %+v \n", err)
-		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
-	}
-
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
 }
 
 // Landau-Lifshitz torque with precession disabled.
 // Used by engine.Relax().
 func LLNoPrecess(torque, m, B *data.Slice) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		llnoprocess__(torque, m, B, wg)
+	} else {
+		go llnoprocess__(torque, m, B, wg)
+	}
+	wg.Wait()
+}
+
+func llnoprocess__(torque, m, B *data.Slice, wg_ sync.WaitGroup) {
+	torque.Lock(X)
+	torque.Lock(Y)
+	torque.Lock(Z)
+	defer torque.Unlock(X)
+	defer torque.Unlock(Y)
+	defer torque.Unlock(Z)
+	m.RLock(X)
+	m.RLock(Y)
+	m.RLock(Z)
+	defer m.RUnlock(X)
+	defer m.RUnlock(Y)
+	defer m.RUnlock(Z)
+	B.RLock(X)
+	B.RLock(Y)
+	B.RLock(Z)
+	defer B.RUnlock(X)
+	defer B.RUnlock(Y)
+	defer B.RUnlock(Z)
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("llnoprecess failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := torque.Len()
 	cfg := make1DConf(N)
 
-	eventList := []*cl.Event{}
-	tmpEvtL := torque.GetAllEvents(X)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Y)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Z)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvt := m.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	if len(eventList) == 0 {
-		eventList = nil
-	}
-
 	event := k_llnoprecess_async(torque.DevPtr(X), torque.DevPtr(Y), torque.DevPtr(Z),
 		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
-		B.DevPtr(X), B.DevPtr(Y), B.DevPtr(Z), N, cfg,
-		eventList)
+		B.DevPtr(X), B.DevPtr(Y), B.DevPtr(Z), N, cfg, cmdqueue,
+		nil)
 
-	torque.SetEvent(X, event)
-	torque.SetEvent(Y, event)
-	torque.SetEvent(Z, event)
-
-	glist := []GSlice{m, B}
-	InsertEventIntoGSlices(event, glist)
+	wg_.Done()
 
 	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 		fmt.Printf("WaitForEvents failed in llnoprecess: %+v \n", err)
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
 	}
-
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
 }
