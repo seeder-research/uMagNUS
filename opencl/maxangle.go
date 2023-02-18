8f63a9f7722ac61d303e70b4ca9215197079be85
Xuanyao Fong <xuanyao.fong@gmail.com>
2023-02-18 16:02:56 +0800

    update maxangle.go

--- opencl/maxangle.go
+++ opencl/maxangle.go
@@ -2,6 +2,7 @@ package opencl
 
 import (
 	"fmt"
+	"sync"
 	"unsafe"
 
 	cl "github.com/seeder-research/uMagNUS/cl"
@@ -11,61 +12,49 @@ import (
 // SetMaxAngle sets dst to the maximum angle of each cells magnetization with all of its neighbors,
 // provided the exchange stiffness with that neighbor is nonzero.
 func SetMaxAngle(dst, m *data.Slice, Aex_red SymmLUT, regions *Bytes, mesh *data.Mesh) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		maxangle__(dst, m, Aex_red, regions, mesh, wg)
+	} else {
+		go maxangle__(dst, m, Aex_red, regions, mesh, wg)
+	}
+	wg.Wait()
+}
+
+func maxangle__(dst, m *data.Slice, Aex_red SymmLUT, regions *Bytes, mesh *data.Mesh, wg_ sync.WaitGroup) {
+	dst.Lock(X)
+	defer dst.Unlock(X)
+	m.RLock(X)
+	m.RLock(Y)
+	m.RLock(Z)
+	defer m.RUnlock(X)
+	defer m.RUnlock(Y)
+	defer m.RUnlock(Z)
+	regions.RLock()
+	defer regions.RUnLock()
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("setmaxangle failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := mesh.Size()
 	pbc := mesh.PBC_code()
 	cfg := make3DConf(N)
 
-	eventList := [](*cl.Event){}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvt := m.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = regions.GetEvent()
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	if len(eventList) == 0 {
-		eventList = nil
-	}
-
 	event := k_setmaxangle_async(dst.DevPtr(0),
 		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
 		unsafe.Pointer(Aex_red), regions.Ptr,
-		N[X], N[Y], N[Z], pbc, cfg, eventList)
+		N[X], N[Y], N[Z], pbc, cfg,
+		cmdqueue, nil)
 
-	dst.SetEvent(0, event)
+	wg_.Done()
 
-	glist := []GSlice{m}
-	InsertEventIntoGSlices(event, glist)
-	regions.InsertReadEvent(event)
-
-	if Debug {
-		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
-			fmt.Printf("WaitForEvents failed in setmaxangle: %+v \n", err)
-		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		regions.RemoveReadEvent(event)
-		return
+	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents failed in setmaxangle: %+v \n", err)
 	}
-
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents failed in setmaxangle: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, regions)
-
 }
