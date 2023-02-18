bb9fa2579aee837a883e24716abacbcf48e2b678
Xuanyao Fong <xuanyao.fong@gmail.com>
2023-02-18 22:27:55 +0800

    update shift.go

--- opencl/shift.go
+++ opencl/shift.go
@@ -2,6 +2,7 @@ package opencl
 
 import (
 	"fmt"
+	"sync"
 
 	cl "github.com/seeder-research/uMagNUS/cl"
 	data "github.com/seeder-research/uMagNUS/data"
@@ -13,197 +14,199 @@ import (
 func ShiftX(dst, src *data.Slice, shiftX int, clampL, clampR float32) {
 	util.Argument(dst.NComp() == 1 && src.NComp() == 1)
 	util.Assert(dst.Len() == src.Len())
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		shiftx__(dst, src, shiftX, clampL, clampR, wg)
+	} else {
+		go shiftx__(dst, src, shiftX, clampL, clampR, wg)
+	}
+	wg.Wait()
+}
+
+func shiftx__(dst, src *data.Slice, shiftX int, clampL, clampR float32, wg_ sync.WaitGroup) {
+	dst.Lock(0)
+	defer dst.Unlock(0)
+	src.RLock(0)
+	defer src.RUnlock(0)
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("shiftx failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := dst.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := src.GetEvent(0)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
 	event := k_shiftx_async(dst.DevPtr(0), src.DevPtr(0), N[X], N[Y], N[Z], shiftX, clampL, clampR, cfg,
-		eventsList)
+		cmdqueue, nil)
 
-	dst.SetEvent(0, event)
+	wg_.Done()
 
-	glist := []GSlice{src}
-	InsertEventIntoGSlices(event, glist)
-
-	if Debug {
-		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
-			fmt.Printf("WaitForEvents in shiftx failed: %+v \n", err)
-		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
+	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents in shiftx failed: %+v \n", err)
 	}
-
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
 }
 
 func ShiftY(dst, src *data.Slice, shiftY int, clampL, clampR float32) {
 	util.Argument(dst.NComp() == 1 && src.NComp() == 1)
 	util.Assert(dst.Len() == src.Len())
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		shifty__(dst, src, shiftY, clampL, clampR, wg)
+	} else {
+		go shifty__(dst, src, shiftY, clampL, clampR, wg)
+	}
+	wg.Wait()
+}
+
+func shifty__(dst, src *data.Slice, shiftY int, clampL, clampR float32, wg_ sync.WaitGroup) {
+	dst.Lock(0)
+	defer dst.Unlock(0)
+	src.RLock(0)
+	defer src.RUnlock(0)
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("shifty failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := dst.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := src.GetEvent(0)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
 	event := k_shifty_async(dst.DevPtr(0), src.DevPtr(0), N[X], N[Y], N[Z], shiftY, clampL, clampR, cfg,
-		eventsList)
+		cmdqueue, nil)
 
-	dst.SetEvent(0, event)
+	wg_.Done()
 
-	glist := []GSlice{src}
-	InsertEventIntoGSlices(event, glist)
-
-	if Debug {
-		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
-			fmt.Printf("WaitForEvents in shifty failed: %+v \n", err)
-		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
+	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents in shifty failed: %+v \n", err)
 	}
-
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
 }
 
 func ShiftZ(dst, src *data.Slice, shiftZ int, clampL, clampR float32) {
 	util.Argument(dst.NComp() == 1 && src.NComp() == 1)
 	util.Assert(dst.Len() == src.Len())
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		shiftz__(dst, src, shiftZ, clampL, clampR, wg)
+	} else {
+		go shiftz__(dst, src, shiftZ, clampL, clampR, wg)
+	}
+	wg.Wait()
+}
+
+func shiftz__(dst, src *data.Slice, shiftZ int, clampL, clampR float32, wg_ sync.WaitGroup) {
+	dst.Lock(0)
+	defer dst.Unlock(0)
+	src.RLock(0)
+	defer src.RUnlock(0)
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("shiftz failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := dst.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := src.GetEvent(0)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
 	event := k_shiftz_async(dst.DevPtr(0), src.DevPtr(0), N[X], N[Y], N[Z], shiftZ, clampL, clampR, cfg,
-		eventsList)
+		cmdqueue, nil)
 
-	dst.SetEvent(0, event)
+	wg_.Done()
 
-	glist := []GSlice{src}
-	InsertEventIntoGSlices(event, glist)
-
-	if Debug {
-		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
-			fmt.Printf("WaitForEvents in shiftz failed: %+v \n", err)
-		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
+	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents in shiftz failed: %+v \n", err)
 	}
-
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
 }
 
 // Like Shift, but for bytes
 func ShiftBytes(dst, src *Bytes, m *data.Mesh, shiftX int, clamp byte) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		shiftbytes__(dst, src, m, shiftX, clamp, wg)
+	} else {
+		go shiftbytes__(dst, src, m, shiftX, clamp, wg)
+	}
+	wg.Wait()
+}
+
+func shiftbytes__(dst, src *Bytes, m *data.Mesh, shiftX int, clamp byte, wg_ sync.WaitGroup) {
+	dst.Lock()
+	defer dst.Unlock()
+	src.RLock()
+	defer src.RUnlock()
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("shiftbytes failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := m.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents()
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
+	event := k_shiftbytes_async(dst.Ptr, src.Ptr, N[X], N[Y], N[Z], shiftX, clamp, cfg,
+		cmdqueue, nil)
+
+	wg_.Done()
+
+	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents in shiftbytes failed: %+v \n", err)
 	}
-	tmpEvt := src.GetEvent()
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
-	event := k_shiftbytes_async(dst.Ptr, src.Ptr, N[X], N[Y], N[Z], shiftX, clamp, cfg, nil)
-
-	dst.SetEvent(event)
-
-	src.InsertReadEvent(event)
-
-	if Debug {
-		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
-			fmt.Printf("WaitForEvents in shiftbytes failed: %+v \n", err)
-		}
-		src.RemoveReadEvent(event)
-		return
-	}
-
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents in shiftbytes failed: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, src)
-
 }
 
 func ShiftBytesY(dst, src *Bytes, m *data.Mesh, shiftY int, clamp byte) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		shiftbytesy__(dst, src, m, shiftY, clamp, wg)
+	} else {
+		go shiftbytesy__(dst, src, m, shiftY, clamp, wg)
+	}
+	wg.Wait()
+}
+
+func shiftbytesy__(dst, src *Bytes, m *data.Mesh, shiftY int, clamp byte, wg_ sync.WaitGroup) {
+	dst.Lock()
+	defer dst.Unlock()
+	src.RLock()
+	defer src.RUnlock()
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("shiftbytes failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := m.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents()
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
+	event := k_shiftbytesy_async(dst.Ptr, src.Ptr, N[X], N[Y], N[Z], shiftY, clamp, cfg,
+		cmdqueue, nil)
+
+	wg_.Done()
+
+	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents in shiftbytesy failed: %+v \n", err)
 	}
-	tmpEvt := src.GetEvent()
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
-	event := k_shiftbytesy_async(dst.Ptr, src.Ptr, N[X], N[Y], N[Z], shiftY, clamp, cfg, nil)
-
-	dst.SetEvent(event)
-
-	src.InsertReadEvent(event)
-
-	if Debug {
-		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
-			fmt.Printf("WaitForEvents in shiftbytesy failed: %+v \n", err)
-		}
-		src.RemoveReadEvent(event)
-		return
-	}
-
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents in shiftbytesy failed: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, src)
-
 }
