2632df8e07d6af7bea99b9c12001035151db2c93
Xuanyao Fong <xuanyao.fong@gmail.com>
2023-02-18 17:13:35 +0800

    update regionspintorque.go

--- opencl/regionspintorque.go
+++ opencl/regionspintorque.go
@@ -5,12 +5,57 @@ package opencl
 import (
 	"fmt"
 	"math"
+	"sync"
 
 	cl "github.com/seeder-research/uMagNUS/cl"
 	data "github.com/seeder-research/uMagNUS/data"
 )
 
 func AddRegionSpinTorque(torque, m *data.Slice, Msat MSlice, regions *Bytes, regionA, regionB uint8, sX, sY, sZ int, J, alpha, pfix, pfree, λfix, λfree, ε_prime float32, mesh *data.Mesh) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		addregionspintorque__(torque, m, Msat, regions, regionA, regionB,
+			sX, sY, sZ, J, alpha, pfix, pfree, λfix, λfree, ε_prime,
+			mesh, wg)
+	} else {
+		go addregionspintorque__(torque, m, Msat, regions, regionA, regionB,
+			sX, sY, sZ, J, alpha, pfix, pfree, λfix, λfree, ε_prime,
+			mesh, wg)
+	}
+	wg.Wait()
+}
+
+func addregionspintorque__(torque, m *data.Slice, Msat MSlice, regions *Bytes, regionA, regionB uint8, sX, sY, sZ int, J, alpha, pfix, pfree, λfix, λfree, ε_prime float32, mesh *data.Mesh, wg_ sync.WaitGroup) {
+	torque.Lock(X)
+	torque.Lock(Y)
+	torque.Lock(Z)
+	defer torque.Unlock(X)
+	defer torque.Unlock(Y)
+	defer torque.Unlock(Z)
+	m.RLock(X)
+	m.RLock(Y)
+	m.RLock(Z)
+	defer m.RUnlock(X)
+	defer m.RUnlock(Y)
+	defer m.RUnlock(Z)
+	if Msat.GetSlicePtr() != nil {
+		Msat.RLock()
+		defer Msat.RUnlock()
+	}
+	if regions != nil {
+		regions.RLock()
+		defer regions.RUnlock()
+	}
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("addtworegionoommfslonczewskitorque failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	c := mesh.CellSize()
 	dX := float64(sX) * c[X]
 	dY := float64(sY) * c[Y]
@@ -25,80 +70,17 @@ func AddRegionSpinTorque(torque, m *data.Slice, Msat MSlice, regions *Bytes, reg
 	N := mesh.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := torque.GetAllEvents(X)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Y)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Z)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := m.GetEvent(X)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Y)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Z)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if Msat.GetSlicePtr() != nil {
-		tmpEvt = Msat.GetEvent(0)
-		if tmpEvt != nil {
-			eventsList = append(eventsList, tmpEvt)
-		}
-	}
-	tmpEvt = regions.GetEvent()
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
 	event := k_addtworegionoommfslonczewskitorque_async(torque.DevPtr(X), torque.DevPtr(Y), torque.DevPtr(Z),
 		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
 		Msat.DevPtr(0), Msat.Mul(0),
 		regions.Ptr, regionA, regionB,
 		sX, sY, sZ, N[X], N[Y], N[Z],
 		J, alpha, pfix, pfree, λfix, λfree, ε_prime, float32(cellwgt),
-		cfg,
-		eventsList)
+		cfg, cmdqueue, nil)
 
-	torque.SetEvent(X, event)
-	torque.SetEvent(Y, event)
-	torque.SetEvent(Z, event)
+	wg_.Done()
 
-	glist := []GSlice{m}
-	if Msat.GetSlicePtr() != nil {
-		glist = append(glist, Msat)
+	if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents failed in addtworegionoommfslonczewskitorque: %+v", err)
 	}
-	InsertEventIntoGSlices(event, glist)
-	regions.InsertReadEvent(event)
-
-	if Debug {
-		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
-			fmt.Printf("WaitForEvents failed in addtworegionoommfslonczewskitorque: %+v", err)
-		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		regions.RemoveReadEvent(event)
-		return
-	}
-
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents failed in addtworegionoommfslonczewskitorque: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, regions)
-
 }
