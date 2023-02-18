48c0bd2a9d5d65a66f4ba61ea0ed967eeb5145ad
Xuanyao Fong <xuanyao.fong@gmail.com>
2023-02-18 23:14:20 +0800

    update vecnorm.go, zeromask.go and zhangli.go

--- opencl/zeromask.go
+++ opencl/zeromask.go
@@ -2,6 +2,7 @@ package opencl
 
 import (
 	"fmt"
+	"sync"
 	"unsafe"
 
 	cl "github.com/seeder-research/uMagNUS/cl"
@@ -10,39 +11,43 @@ import (
 
 // Sets vector dst to zero where mask != 0.
 func ZeroMask(dst *data.Slice, mask LUTPtr, regions *Bytes) {
+	var wg syncWaitGroup
+	for c := 0; c < dst.NComp(); c++ {
+		wg.Add(1)
+		if Synchronous {
+			zeromask__(dst, mask, regions, c, wg)
+		} else {
+			go zeromask__(dst, mask, regions, c, wg)
+		}
+	}
+	wg.Wait()
+}
+
+func zeromask__(dst *data.Slice, mask LUTPtr, regions *Bytes, c int, wg_ sync.WaitGroup) {
+	dst.Lock(c)
+	defer dst.Unlock(c)
+	if regions != nil {
+		regions.RLock()
+		defer regions.RUnlock()
+	}
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("zeromask failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	N := dst.Len()
 	cfg := make1DConf(N)
 
-	eventList := make([]*cl.Event, dst.NComp())
-	for c := 0; c < dst.NComp(); c++ {
-		intEventList := []*cl.Event{}
-		tmpEvtL := dst.GetAllEvents(c)
-		if len(tmpEvtL) > 0 {
-			intEventList = append(intEventList, tmpEvtL...)
-		}
-		tmpEvt := regions.GetEvent()
-		if tmpEvt != nil {
-			intEventList = append(intEventList, tmpEvt)
-		}
-		if len(intEventList) == 0 {
-			intEventList = nil
-		}
-		eventList[c] = k_zeromask_async(dst.DevPtr(c), unsafe.Pointer(mask), regions.Ptr, N, cfg, intEventList)
+	event = k_zeromask_async(dst.DevPtr(c), unsafe.Pointer(mask), regions.Ptr, N, cfg,
+		cmdqueue, nil)
 
-		dst.SetEvent(c, eventList[c])
+	wg_.Done()
 
-		regions.InsertReadEvent(eventList[c])
-		go func(ev *cl.Event, b *Bytes) {
-			if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-				fmt.Printf("WaitForEvents failed in zeromask: %+v \n", err)
-			}
-			b.RemoveReadEvent(ev)
-		}(eventList[c], regions)
-	}
-
-	if Debug {
-		if err := cl.WaitForEvents(eventList); err != nil {
-			fmt.Printf("WaitForEvents failed in zeromask: %+v \n", err)
-		}
+	if err = cl.WaitForEvents([]*cl.Event{ev}); err != nil {
+		fmt.Printf("WaitForEvents failed in zeromask: %+v \n", err)
 	}
 }
--- opencl/zhangli.go
+++ opencl/zhangli.go
@@ -2,6 +2,7 @@ package opencl
 
 import (
 	"fmt"
+	"sync"
 
 	cl "github.com/seeder-research/uMagNUS/cl"
 	data "github.com/seeder-research/uMagNUS/data"
@@ -10,68 +11,61 @@ import (
 // Add Zhang-Li ST torque (Tesla) to torque.
 // see zhangli2.cl
 func AddZhangLiTorque(torque, m *data.Slice, Msat, J, alpha, xi, pol MSlice, mesh *data.Mesh) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if Synchronous {
+		addzhanglitorque__(torque, m, Msat, J, alpha, xi, pol, mesh, wg)
+	} else {
+		go addzhanglitorque__(torque, m, Msat, J, alpha, xi, pol, mesh, wg)
+	}
+	wg.Wait()
+}
+
+func addzhanglitorque__(torque, m *data.Slice, Msat, J, alpha, xi, pol MSlice, mesh *data.Mesh, wg_ sync.WaitGroup) {
+	torque.Lock(X)
+	torque.Lock(Y)
+	torque.Lock(Z)
+	defer torque.Unlock(X)
+	defer torque.Unlock(Y)
+	defer torque.Unlock(Z)
+	m.RLock(X)
+	m.RLock(Y)
+	m.RLock(Z)
+	defer m.RUnlock(X)
+	defer m.RUnlock(Y)
+	defer m.RUnlock(Z)
+	if J.GetSlicePtr() != nil {
+		J.RLock()
+		defer J.RUnlock()
+	}
+	if alpha.GetSlicePtr() != nil {
+		alpha.RLock()
+		defer alpha.RUnlock()
+	}
+	if Msat.GetSlicePtr() != nil {
+		Msat.RLock()
+		defer Msat.RUnlock()
+	}
+	if pol.GetSlicePtr() != nil {
+		pol.RLock()
+		defer pol.RUnlock()
+	}
+	if xi.GetSlicePtr() != nil {
+		xi.RLock()
+		defer xi.RUnlock()
+	}
+
+	// Create the command queue to execute the command
+	cmdqueue, err := ClCtx.CreateCommandQueue(ClDevice, 0)
+	if err != nil {
+		fmt.Printf("addzhanglitorque failed to create command queue: %+v \n", err)
+		return nil
+	}
+	defer cmdqueue.Release()
+
 	c := mesh.CellSize()
 	N := mesh.Size()
 	cfg := make3DConf(N)
-
-	eventList := [](*cl.Event){}
-	tmpEvtL := torque.GetAllEvents(X)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Y)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Z)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvt := m.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	if J.GetSlicePtr() != nil {
-		tmpEvt = J.GetEvent(Z)
-		if tmpEvt != nil {
-			eventList = append(eventList, tmpEvt)
-		}
-	}
-	if alpha.GetSlicePtr() != nil {
-		tmpEvt = alpha.GetEvent(0)
-		if tmpEvt != nil {
-			eventList = append(eventList, tmpEvt)
-		}
-	}
-	if Msat.GetSlicePtr() != nil {
-		tmpEvt = Msat.GetEvent(0)
-		if tmpEvt != nil {
-			eventList = append(eventList, tmpEvt)
-		}
-	}
-	if xi.GetSlicePtr() != nil {
-		tmpEvt = xi.GetEvent(0)
-		if tmpEvt != nil {
-			eventList = append(eventList, tmpEvt)
-		}
-	}
-	if pol.GetSlicePtr() != nil {
-		tmpEvt = pol.GetEvent(0)
-		if tmpEvt != nil {
-			eventList = append(eventList, tmpEvt)
-		}
-	}
-	if len(eventList) == 0 {
-		eventList = nil
-	}
 
 	event := k_addzhanglitorque2_async(
 		torque.DevPtr(X), torque.DevPtr(Y), torque.DevPtr(Z),
@@ -85,38 +79,11 @@ func AddZhangLiTorque(torque, m *data.Slice, Msat, J, alpha, xi, pol MSlice, mes
 		pol.DevPtr(0), pol.Mul(0),
 		float32(c[X]), float32(c[Y]), float32(c[Z]),
 		N[X], N[Y], N[Z], mesh.PBC_code(), cfg,
-		eventList)
+		cmdqueue, nil)
 
-	torque.SetEvent(X, event)
-	torque.SetEvent(Y, event)
-	torque.SetEvent(Z, event)
+	wg_.Done()
 
-	glist := []GSlice{m}
-	if J.GetSlicePtr() != nil {
-		glist = append(glist, J)
+	if err = cl.WaitForEvents([](*cl.Event){event}); err != nil {
+		fmt.Printf("WaitForEvents failed in addzhanglitorque: %+v \n", err)
 	}
-	if Msat.GetSlicePtr() != nil {
-		glist = append(glist, Msat)
-	}
-	if alpha.GetSlicePtr() != nil {
-		glist = append(glist, alpha)
-	}
-	if xi.GetSlicePtr() != nil {
-		glist = append(glist, xi)
-	}
-	if pol.GetSlicePtr() != nil {
-		glist = append(glist, pol)
-	}
-	InsertEventIntoGSlices(event, glist)
-
-	if Debug {
-		if err := cl.WaitForEvents([]*cl.Event{event}); err != nil {
-			fmt.Printf("WaitForEvents failed in addzhanglitorque: %+v \n", err)
-		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
-	}
-
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
 }
