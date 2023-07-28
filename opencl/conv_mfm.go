94f583ed764b6ccd430c2a2109aa051da16c0ed2
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2023-07-29 00:16:35 +0800

    midway in conv_mfm

--- opencl/conv_mfm.go
+++ opencl/conv_mfm.go
@@ -14,11 +14,11 @@ type MFMConvolution struct {
 	size        [3]int         // 3D size of the input/output data
 	kernSize    [3]int         // Size of kernel and logical FFT size.
 	fftKernSize [3]int         //
-	fftRBuf     *data.Slice    // FFT input buf for FFT, shares storage with fftCBuf.
-	fftCBuf     *data.Slice    // FFT output buf, shares storage with fftRBuf
+	fftRBuf     [3]*data.Slice    // FFT input buf for FFT, shares storage with fftCBuf.
+	fftCBuf     [3]*data.Slice    // FFT output buf, shares storage with fftRBuf
 	gpuFFTKern  [3]*data.Slice // FFT kernel on device
-	fwPlan      fft3DR2CPlan   // Forward FFT (1 component)
-	bwPlan      fft3DC2RPlan   // Backward FFT (1 component)
+	fwPlan      [3]fft3DR2CPlan   // Forward FFT (1 component)
+	bwPlan      [3]fft3DC2RPlan   // Backward FFT (1 component)
 	kern        [3]*data.Slice // Real-space kernel (host)
 	mesh        *data.Mesh
 }
@@ -29,29 +29,37 @@ func (c *MFMConvolution) Free() {
 	}
 	c.size = [3]int{}
 	c.kernSize = [3]int{}
-	c.fftCBuf.Free() // shared with fftRbuf
-	c.fftCBuf = nil
-	c.fftRBuf = nil
 
 	for j := 0; j < 3; j++ {
 		c.gpuFFTKern[j].Free()
 		c.gpuFFTKern[j] = nil
 		c.kern[j] = nil
+		c.fwPlan[j].Free()
+		c.bwPlan[j].Free()
+		c.fftCBuf[i].Free() // shared with fftRbuf
+		c.fftCBuf[i] = nil
+		c.fftRBuf[i] = nil
 	}
-	c.fwPlan.Free()
-	c.bwPlan.Free()
 }
 
 func (c *MFMConvolution) init() {
 	// init FFT plans
 	padded := c.kernSize
-	c.fwPlan = newFFT3DR2C(padded[X], padded[Y], padded[Z])
-	c.bwPlan = newFFT3DC2R(padded[X], padded[Y], padded[Z])
+	c.fwPlan[X] = newFFT3DR2C(padded[X], padded[Y], padded[Z])
+	c.fwPlan[Y] = newFFT3DR2C(padded[X], padded[Y], padded[Z])
+	c.fwPlan[Z] = newFFT3DR2C(padded[X], padded[Y], padded[Z])
+	c.bwPlan[X] = newFFT3DC2R(padded[X], padded[Y], padded[Z])
+	c.bwPlan[Y] = newFFT3DC2R(padded[X], padded[Y], padded[Z])
+	c.bwPlan[Z] = newFFT3DC2R(padded[X], padded[Y], padded[Z])
 
 	// init device buffers
 	nc := fftR2COutputSizeFloats(c.kernSize)
-	c.fftCBuf = NewSlice(1, nc)
-	c.fftRBuf = NewSlice(1, c.kernSize)
+	c.fftCBuf[X] = NewSlice(1, nc)
+	c.fftCBuf[Y] = NewSlice(1, nc)
+	c.fftCBuf[Z] = NewSlice(1, nc)
+	c.fftRBuf[X] = NewSlice(1, c.kernSize)
+	c.fftRBuf[Y] = NewSlice(1, c.kernSize)
+	c.fftRBuf[Z] = NewSlice(1, c.kernSize)
 
 	c.gpuFFTKern[X] = NewSlice(1, nc)
 	c.gpuFFTKern[Y] = NewSlice(1, nc)
@@ -64,35 +72,50 @@ func (c *MFMConvolution) initFFTKern3D() {
 	c.fftKernSize = fftR2COutputSizeFloats(c.kernSize)
 
 	for i := 0; i < 3; i++ {
-		zero1_async(c.fftRBuf)
-		data.Copy(c.fftRBuf, c.kern[i])
-		err := c.fwPlan.ExecAsync(c.fftRBuf, c.fftCBuf)
+		tmpQueue := c.fwPlan[i].GetCommandQueue()
+		zero1_async(c.fftRBuf[i], tmpQueue, []*cl.Event{})
+		data.Copy(c.fftRBuf[i], c.kern[i])
+		err := c.fwPlan[i].ExecAsync(c.fftRBuf[i], c.fftCBuf[i])
 		if err != nil {
 			fmt.Printf("error enqueuing forward fft in initfftkern3d: %+v \n", err)
 		}
-		scale := 2 / float32(c.fwPlan.InputLen()) // ??
-		zero1_async(c.gpuFFTKern[i])
-		Madd2(c.gpuFFTKern[i], c.gpuFFTKern[i], c.fftCBuf, 0, scale)
+		scale := 2 / float32(c.fwPlan[i].InputLen()) // ??
+
+		// Checkout new queue for zero1 and launch
+		zq := qm.CheckoutQueue(CmdQueuePool, nil)
+		zero1_async(c.gpuFFTKern[i], zq, []*cl.Event{})
+
+		// Get marker for synchronizing madd2
+		var ev1 *cl.Event
+		ev1, err = zq.EnqueueMarkerWithWaitList({}*cl.Event{})
+		if err != nil {
+			fmt.Printf("Failed to enqueue marker in initFFTKern3d: %+v \n", err)
+		}
+
+		// Checkin queue post execution
+		qwg := qm.NewQueueWaitGroup(zq, nil)
+		ReturnQueuePool <- qwg
+		Madd2(c.gpuFFTKern[i], c.gpuFFTKern[i], c.fftCBuf[i], 0, scale, tmpQueue, []*cl.Event{ev1})
 	}
 }
 
 // store MFM image in output, based on magnetization in inp.
 func (c *MFMConvolution) Exec(outp, inp, vol *data.Slice, Msat MSlice) {
 	for i := 0; i < 3; i++ {
-		zero1_async(c.fftRBuf)
-		copyPadMul(c.fftRBuf, inp.Comp(i), vol, c.kernSize, c.size, Msat)
+		zero1_async(c.fftRBuf[i])
+		copyPadMul(c.fftRBuf[i], inp.Comp(i), vol, c.kernSize, c.size, Msat)
 		var err error
-		if err = c.fwPlan.ExecAsync(c.fftRBuf, c.fftCBuf); err != nil {
+		if err = c.fwPlan[i].ExecAsync(c.fftRBuf[i], c.fftCBuf[i]); err != nil {
 			fmt.Printf("error enqueuing forward fft in mfmconv exec: %+v \n", err)
 		}
 
 		Nx, Ny := c.fftKernSize[X]/2, c.fftKernSize[Y] //   ??
-		kernMulC_async(c.fftCBuf, c.gpuFFTKern[i], Nx, Ny)
+		kernMulC_async(c.fftCBuf[i], c.gpuFFTKern[i], Nx, Ny)
 
-		if err = c.bwPlan.ExecAsync(c.fftCBuf, c.fftRBuf); err != nil {
+		if err = c.bwPlan.ExecAsync(c.fftCBuf[i], c.fftRBuf[i]); err != nil {
 			fmt.Printf("error enqueuing backward fft in mfmconv exec: %+v \n", err)
 		}
-		copyUnPad(outp.Comp(i), c.fftRBuf, c.size, c.kernSize)
+		copyUnPad(outp.Comp(i), c.fftRBuf[i], c.size, c.kernSize)
 	}
 }
 
