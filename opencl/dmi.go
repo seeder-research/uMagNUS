63207b659c9e509589e56c84f1d5bff9d104fc2a
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2023-07-29 23:23:41 +0800

    update dmi

--- opencl/dmi.go
+++ opencl/dmi.go
@@ -12,7 +12,8 @@ import (
 // Add effective field of Dzyaloshinskii-Moriya interaction to Beff (Tesla).
 // According to Bagdanov and Röβler, PRL 87, 3, 2001. eq.8 (out-of-plane symmetry breaking).
 // See dmi.cl
-func AddDMI(Beff *data.Slice, m *data.Slice, Aex_red, Dex_red SymmLUT, Msat MSlice, regions *Bytes, mesh *data.Mesh, OpenBC bool) {
+func AddDMI(Beff *data.Slice, m *data.Slice, Aex_red, Dex_red SymmLUT, Msat MSlice, regions *Bytes, mesh *data.Mesh, OpenBC bool,
+	q *cl.CommandQueue, ewl []*cl.Event) {
 	cellsize := mesh.CellSize()
 	N := Beff.Size()
 	util.Argument(m.Size() == N)
@@ -22,45 +23,13 @@ func AddDMI(Beff *data.Slice, m *data.Slice, Aex_red, Dex_red SymmLUT, Msat MSli
 		openBC = 1
 	}
 
-	eventWaitList := []*cl.Event{}
-	tmpEventL := Beff.GetAllEvents(X)
-	if len(tmpEventL) > 0 {
-		eventWaitList = append(eventWaitList, tmpEventL...)
-	}
-	tmpEventL = Beff.GetAllEvents(Y)
-	if len(tmpEventL) > 0 {
-		eventWaitList = append(eventWaitList, tmpEventL...)
-	}
-	tmpEventL = Beff.GetAllEvents(Z)
-	if len(tmpEventL) > 0 {
-		eventWaitList = append(eventWaitList, tmpEventL...)
-	}
-	tmpEvent := m.GetEvent(X)
-	if tmpEvent != nil {
-		eventWaitList = append(eventWaitList, tmpEvent)
-	}
-	tmpEvent = m.GetEvent(Y)
-	if tmpEvent != nil {
-		eventWaitList = append(eventWaitList, tmpEvent)
-	}
-	tmpEvent = m.GetEvent(Z)
-	if tmpEvent != nil {
-		eventWaitList = append(eventWaitList, tmpEvent)
-	}
-	tmpEvent = regions.GetEvent()
-	if tmpEvent != nil {
-		eventWaitList = append(eventWaitList, tmpEvent)
-	}
-	if len(eventWaitList) == 0 {
-		eventWaitList = nil
-	}
 	event := k_adddmi_async(Beff.DevPtr(X), Beff.DevPtr(Y), Beff.DevPtr(Z),
 		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
 		Msat.DevPtr(0), Msat.Mul(0),
 		unsafe.Pointer(Aex_red), unsafe.Pointer(Dex_red), regions.Ptr,
 		float32(cellsize[X]), float32(cellsize[Y]), float32(cellsize[Z]),
 		N[X], N[Y], N[Z], mesh.PBC_code(), openBC, cfg,
-		eventWaitList)
+		ewl, q)
 
 	Beff.SetEvent(X, event)
 	Beff.SetEvent(Y, event)
@@ -73,21 +42,12 @@ func AddDMI(Beff *data.Slice, m *data.Slice, Aex_red, Dex_red SymmLUT, Msat MSli
 	InsertEventIntoGSlices(event, glist)
 	regions.InsertReadEvent(event)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents failed in adddmi: %+v \n", err)
 		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
 		regions.RemoveReadEvent(event)
-		return
 	}
 
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents failed in adddmi: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, regions)
-
+	return
 }
