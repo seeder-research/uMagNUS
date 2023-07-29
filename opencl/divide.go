a3852ef8e2e4f9c8ef0bc281b0a65474979a3c14
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2023-07-29 23:21:19 +0800

    update divide

--- opencl/divide.go
+++ opencl/divide.go
@@ -10,44 +10,23 @@ import (
 
 // divide: dst[i] = a[i] / b[i]
 // divide by zero automagically returns 0.0
-func Divide(dst, a, b *data.Slice) {
+func Divide(dst, a, b *data.Slice, q []*cl.CommandQueue, ewl []*cl.Event) {
 	N := dst.Len()
 	nComp := dst.NComp()
 	util.Assert(a.Len() == N && a.NComp() == nComp && b.Len() == N && b.NComp() == nComp)
+	util.Assert(NComp == len(q))
 	cfg := make1DConf(N)
-	eventList := make([]*cl.Event, nComp)
+
 	for c := 0; c < nComp; c++ {
-		waitList := []*cl.Event{}
-		tmpEvtL := dst.GetAllEvents(c)
-		if len(tmpEvtL) > 0 {
-			waitList = append(waitList, tmpEvtL...)
-		}
-		tmpEvt := a.GetEvent(c)
-		if tmpEvt != nil {
-			waitList = append(waitList, tmpEvt)
-		}
-		tmpEvt = b.GetEvent(c)
-		if tmpEvt != nil {
-			waitList = append(waitList, tmpEvt)
-		}
-		if len(waitList) == 0 {
-			waitList = nil
-		}
-		eventList[c] = k_divide_async(dst.DevPtr(c), a.DevPtr(c), b.DevPtr(c), N, cfg,
-			waitList)
-		dst.SetEvent(c, eventList[c])
-		a.InsertReadEvent(c, eventList[c])
-		b.InsertReadEvent(c, eventList[c])
-		go func(ev *cl.Event, idx int, sl []*data.Slice) {
-			if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-				fmt.Printf("WaitForEvents failed in crop: %+v \n", err)
+		event := k_divide_async(dst.DevPtr(c), a.DevPtr(c), b.DevPtr(c), N, cfg,
+			ewl, q[c])
+		dst.SetEvent(c, event)
+		a.InsertReadEvent(c, event)
+		b.InsertReadEvent(c, event)
+		if Synchronous {
+			if err := cl.WaitForEvents([]*cl.Event{event}); err != nil {
+				fmt.Printf("WaitForEvents failed in divide (comp %d: %+v \n", c, err)
 			}
-			for _, sa := range sl {
-				sa.RemoveReadEvent(idx, ev)
-			}
-		}(eventList[c], c, []*data.Slice{a, b})
-	}
-	if err := cl.WaitForEvents(eventList); err != nil {
-		fmt.Printf("WaitForEvents failed in divide: %+v \n", err)
+		}
 	}
 }
