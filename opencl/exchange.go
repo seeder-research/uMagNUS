9e9570ca4941e8a2080e412092b64aa3caf86b39
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2023-07-29 23:30:44 +0800

    update lltorque

--- opencl/exchange.go
+++ opencl/exchange.go
@@ -9,11 +9,13 @@ import (
 )
 
 // Add exchange field to Beff.
-// 	m: normalized magnetization
-// 	B: effective field in Tesla
-// 	Aex_red: Aex / (Msat * 1e18 m2)
+//
+//	m: normalized magnetization
+//	B: effective field in Tesla
+//	Aex_red: Aex / (Msat * 1e18 m2)
+//
 // see exchange.cl
-func AddExchange(B, m *data.Slice, Aex_red SymmLUT, Msat MSlice, regions *Bytes, mesh *data.Mesh) {
+func AddExchange(B, m *data.Slice, Aex_red SymmLUT, Msat MSlice, regions *Bytes, mesh *data.Mesh, q *cl.CommandQueue, ewl []*cl.Event) {
 	c := mesh.CellSize()
 	wx := float32(2 / (c[X] * c[X]))
 	wy := float32(2 / (c[Y] * c[Y]))
@@ -22,51 +24,12 @@ func AddExchange(B, m *data.Slice, Aex_red SymmLUT, Msat MSlice, regions *Bytes,
 	pbc := mesh.PBC_code()
 	cfg := make3DConf(N)
 
-	eventList := []*cl.Event{}
-	tmpEvtL := B.GetAllEvents(X)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = B.GetAllEvents(Y)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = B.GetAllEvents(Z)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvt := m.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	if Msat.GetSlicePtr() != nil {
-		tmpEvt = Msat.GetEvent(0)
-		if tmpEvt != nil {
-			eventList = append(eventList, tmpEvt)
-		}
-	}
-	tmpEvt = regions.GetEvent()
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	if len(eventList) == 0 {
-		eventList = nil
-	}
-
 	event := k_addexchange_async(B.DevPtr(X), B.DevPtr(Y), B.DevPtr(Z),
 		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
 		Msat.DevPtr(0), Msat.Mul(0),
 		unsafe.Pointer(Aex_red), regions.Ptr,
 		wx, wy, wz, N[X], N[Y], N[Z], pbc, cfg,
-		eventList)
+		ewl, q)
 
 	B.SetEvent(X, event)
 	B.SetEvent(Y, event)
@@ -79,27 +42,18 @@ func AddExchange(B, m *data.Slice, Aex_red SymmLUT, Msat MSlice, regions *Bytes,
 	InsertEventIntoGSlices(event, glist)
 	regions.InsertReadEvent(event)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents failed in addexchange: %+v", err)
 		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
 		regions.RemoveReadEvent(event)
-		return
 	}
 
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents failed in addexchange: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, regions)
-
+	return
 }
 
 // Finds the average exchange strength around each cell, for debugging.
-func ExchangeDecode(dst *data.Slice, Aex_red SymmLUT, regions *Bytes, mesh *data.Mesh) {
+func ExchangeDecode(dst *data.Slice, Aex_red SymmLUT, regions *Bytes, mesh *data.Mesh, q *cl.CommandQueue, ewl []*cl.Event) {
 	c := mesh.CellSize()
 	wx := float32(2 / (c[X] * c[X]))
 	wy := float32(2 / (c[Y] * c[Y]))
@@ -108,40 +62,20 @@ func ExchangeDecode(dst *data.Slice, Aex_red SymmLUT, regions *Bytes, mesh *data
 	pbc := mesh.PBC_code()
 	cfg := make3DConf(N)
 
-	eventList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvt := regions.GetEvent()
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	if len(eventList) == 0 {
-		eventList = nil
-	}
-
 	event := k_exchangedecode_async(dst.DevPtr(0), unsafe.Pointer(Aex_red), regions.Ptr,
 		wx, wy, wz, N[X], N[Y], N[Z], pbc, cfg,
-		eventList)
+		ewl, q)
 
 	dst.SetEvent(0, event)
 
 	regions.InsertReadEvent(event)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents failed in exchangedecode: %+v", err)
 		}
 		regions.RemoveReadEvent(event)
-		return
 	}
 
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents failed in exchangedecode: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, regions)
-
+	return
 }
--- opencl/lltorque.go
+++ opencl/lltorque.go
@@ -13,62 +13,15 @@ import (
 // 	m normalized
 // 	B in Tesla
 // see lltorque.cl
-func LLTorque(torque, m, B *data.Slice, alpha MSlice) {
+func LLTorque(torque, m, B *data.Slice, alpha MSlice, q *cl.CommandQueue, ewl []*cl.Event) {
 	N := torque.Len()
 	cfg := make1DConf(N)
-
-	eventList := []*cl.Event{}
-	tmpEvtL := torque.GetAllEvents(X)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Y)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvtL = torque.GetAllEvents(Z)
-	if len(tmpEvtL) > 0 {
-		eventList = append(eventList, tmpEvtL...)
-	}
-	tmpEvt := m.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = m.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(X)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(Y)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	tmpEvt = B.GetEvent(Z)
-	if tmpEvt != nil {
-		eventList = append(eventList, tmpEvt)
-	}
-	if alpha.GetSlicePtr() != nil {
-		tmpEvt = alpha.GetEvent(0)
-		if tmpEvt != nil {
-			eventList = append(eventList, tmpEvt)
-		}
-	}
-	if len(eventList) == 0 {
-		eventList = nil
-	}
 
 	event := k_lltorque2_async(torque.DevPtr(X), torque.DevPtr(Y), torque.DevPtr(Z),
 		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
 		B.DevPtr(X), B.DevPtr(Y), B.DevPtr(Z),
 		alpha.DevPtr(0), alpha.Mul(0), N, cfg,
-		eventList)
+		ewl, q)
 
 	torque.SetEvent(X, event)
 	torque.SetEvent(Y, event)
@@ -80,16 +33,13 @@ func LLTorque(torque, m, B *data.Slice, alpha MSlice) {
 	}
 	InsertEventIntoGSlices(event, glist)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents failed in lltorque: %+v \n", err)
 		}
-		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
 	}
 
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
+	return
 }
 
 // Landau-Lifshitz torque with precession disabled.
