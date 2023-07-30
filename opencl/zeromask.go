316efeba65c2e53f31e08611e235ec90cba238a1
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2023-07-30 23:58:42 +0800

    update zeromask

--- opencl/zeromask.go
+++ opencl/zeromask.go
@@ -9,40 +9,22 @@ import (
 )
 
 // Sets vector dst to zero where mask != 0.
-func ZeroMask(dst *data.Slice, mask LUTPtr, regions *Bytes) {
+func ZeroMask(dst *data.Slice, mask LUTPtr, regions *Bytes, q []*cl.CommandQueue, ewl []*cl.Event) {
 	N := dst.Len()
 	cfg := make1DConf(N)
 
-	eventList := make([]*cl.Event, dst.NComp())
 	for c := 0; c < dst.NComp(); c++ {
-		intEventList := []*cl.Event{}
-		tmpEvtL := dst.GetAllEvents(c)
-		if len(tmpEvtL) > 0 {
-			intEventList = append(intEventList, tmpEvtL...)
-		}
-		tmpEvt := regions.GetEvent()
-		if tmpEvt != nil {
-			intEventList = append(intEventList, tmpEvt)
-		}
-		if len(intEventList) == 0 {
-			intEventList = nil
-		}
-		eventList[c] = k_zeromask_async(dst.DevPtr(c), unsafe.Pointer(mask), regions.Ptr, N, cfg, intEventList)
+		event := k_zeromask_async(dst.DevPtr(c), unsafe.Pointer(mask), regions.Ptr, N, cfg, eql, q)
 
 		dst.SetEvent(c, eventList[c])
 
 		regions.InsertReadEvent(eventList[c])
-		go func(ev *cl.Event, b *Bytes) {
-			if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
+		if Synchronous || Debug {
+			if err := cl.WaitForEvents(eventList); err != nil {
 				fmt.Printf("WaitForEvents failed in zeromask: %+v \n", err)
 			}
-			b.RemoveReadEvent(ev)
-		}(eventList[c], regions)
-	}
-
-	if Debug {
-		if err := cl.WaitForEvents(eventList); err != nil {
-			fmt.Printf("WaitForEvents failed in zeromask: %+v \n", err)
 		}
 	}
+
+	return
 }
