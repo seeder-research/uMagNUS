0b63976b6a735ab153d0db49456ce72a2077f32b
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2023-07-30 23:50:29 +0800

    update shift

--- opencl/shift.go
+++ opencl/shift.go
@@ -10,200 +10,113 @@ import (
 
 // shift dst by shx cells (positive or negative) along X-axis.
 // new edge value is clampL at left edge or clampR at right edge.
-func ShiftX(dst, src *data.Slice, shiftX int, clampL, clampR float32) {
+func ShiftX(dst, src *data.Slice, shiftX int, clampL, clampR float32, q *cl.CommandQueue, ewl []*cl.Event) {
 	util.Argument(dst.NComp() == 1 && src.NComp() == 1)
 	util.Assert(dst.Len() == src.Len())
 	N := dst.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := src.GetEvent(0)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
 	event := k_shiftx_async(dst.DevPtr(0), src.DevPtr(0), N[X], N[Y], N[Z], shiftX, clampL, clampR, cfg,
-		eventsList)
+		ewl, q)
 
 	dst.SetEvent(0, event)
 
 	glist := []GSlice{src}
 	InsertEventIntoGSlices(event, glist)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents in shiftx failed: %+v \n", err)
 		}
 		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
 	}
 
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
+	return
 }
 
-func ShiftY(dst, src *data.Slice, shiftY int, clampL, clampR float32) {
+func ShiftY(dst, src *data.Slice, shiftY int, clampL, clampR float32, q *cl.CommandQueue, ewl []*cl.Event) {
 	util.Argument(dst.NComp() == 1 && src.NComp() == 1)
 	util.Assert(dst.Len() == src.Len())
 	N := dst.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := src.GetEvent(0)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
 	event := k_shifty_async(dst.DevPtr(0), src.DevPtr(0), N[X], N[Y], N[Z], shiftY, clampL, clampR, cfg,
-		eventsList)
+		ewl, q)
 
 	dst.SetEvent(0, event)
 
 	glist := []GSlice{src}
 	InsertEventIntoGSlices(event, glist)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents in shifty failed: %+v \n", err)
 		}
 		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
 	}
 
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
+	return
 }
 
-func ShiftZ(dst, src *data.Slice, shiftZ int, clampL, clampR float32) {
+func ShiftZ(dst, src *data.Slice, shiftZ int, clampL, clampR float32, q *cl.CommandQueue, ewl []*cl.Event) {
 	util.Argument(dst.NComp() == 1 && src.NComp() == 1)
 	util.Assert(dst.Len() == src.Len())
 	N := dst.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := src.GetEvent(0)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
 	event := k_shiftz_async(dst.DevPtr(0), src.DevPtr(0), N[X], N[Y], N[Z], shiftZ, clampL, clampR, cfg,
-		eventsList)
+		ewl, q)
 
 	dst.SetEvent(0, event)
 
 	glist := []GSlice{src}
 	InsertEventIntoGSlices(event, glist)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents in shiftz failed: %+v \n", err)
 		}
 		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
 	}
 
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
+	return
 }
 
 // Like Shift, but for bytes
-func ShiftBytes(dst, src *Bytes, m *data.Mesh, shiftX int, clamp byte) {
+func ShiftBytes(dst, src *Bytes, m *data.Mesh, shiftX int, clamp byte, q *cl.CommandQueue, ewl []*cl.Event) {
 	N := m.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents()
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := src.GetEvent()
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
-	event := k_shiftbytes_async(dst.Ptr, src.Ptr, N[X], N[Y], N[Z], shiftX, clamp, cfg, nil)
+	event := k_shiftbytes_async(dst.Ptr, src.Ptr, N[X], N[Y], N[Z], shiftX, clamp, cfg, ewl, q)
 
 	dst.SetEvent(event)
-
 	src.InsertReadEvent(event)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents in shiftbytes failed: %+v \n", err)
 		}
 		src.RemoveReadEvent(event)
-		return
 	}
 
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents in shiftbytes failed: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, src)
-
+	return
 }
 
-func ShiftBytesY(dst, src *Bytes, m *data.Mesh, shiftY int, clamp byte) {
+func ShiftBytesY(dst, src *Bytes, m *data.Mesh, shiftY int, clamp byte, q *cl.CommandQueue, ewl []*cl.Event) {
 	N := m.Size()
 	cfg := make3DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents()
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := src.GetEvent()
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
-	event := k_shiftbytesy_async(dst.Ptr, src.Ptr, N[X], N[Y], N[Z], shiftY, clamp, cfg, nil)
+	event := k_shiftbytesy_async(dst.Ptr, src.Ptr, N[X], N[Y], N[Z], shiftY, clamp, cfg, ewl, q)
 
 	dst.SetEvent(event)
-
 	src.InsertReadEvent(event)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([](*cl.Event){event}); err != nil {
 			fmt.Printf("WaitForEvents in shiftbytesy failed: %+v \n", err)
 		}
 		src.RemoveReadEvent(event)
-		return
 	}
 
-	go func(ev *cl.Event, b *Bytes) {
-		if err := cl.WaitForEvents([]*cl.Event{ev}); err != nil {
-			fmt.Printf("WaitForEvents in shiftbytesy failed: %+v \n", err)
-		}
-		b.RemoveReadEvent(ev)
-	}(event, src)
-
+	return
 }
