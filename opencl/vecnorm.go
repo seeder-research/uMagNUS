687cb31472115c0cd483d964a9ccd20b91b8e0ce
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2023-07-30 23:56:55 +0800

    update vecnorm

--- opencl/vecnorm.go
+++ opencl/vecnorm.go
@@ -9,51 +9,28 @@ import (
 )
 
 // dst = sqrt(dot(a, a)),
-func VecNorm(dst *data.Slice, a *data.Slice) {
+func VecNorm(dst *data.Slice, a *data.Slice, q *cl.CommandQueue, ewl []*cl.Event) {
 	util.Argument(dst.NComp() == 1 && a.NComp() == 3)
 	util.Argument(dst.Len() == a.Len())
 
 	N := dst.Len()
 	cfg := make1DConf(N)
 
-	eventsList := []*cl.Event{}
-	tmpEvtL := dst.GetAllEvents(0)
-	if len(tmpEvtL) > 0 {
-		eventsList = append(eventsList, tmpEvtL...)
-	}
-	tmpEvt := a.GetEvent(X)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	tmpEvt = a.GetEvent(Y)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	tmpEvt = a.GetEvent(Z)
-	if tmpEvt != nil {
-		eventsList = append(eventsList, tmpEvt)
-	}
-	if len(eventsList) == 0 {
-		eventsList = nil
-	}
-
 	event := k_vecnorm_async(dst.DevPtr(0),
 		a.DevPtr(X), a.DevPtr(Y), a.DevPtr(Z),
-		N, cfg, eventsList)
+		N, cfg, ewl, q)
 
 	dst.SetEvent(0, event)
 
 	glist := []GSlice{a}
 	InsertEventIntoGSlices(event, glist)
 
-	if Debug {
+	if Synchronous || Debug {
 		if err := cl.WaitForEvents([]*cl.Event{event}); err != nil {
 			fmt.Printf("WaitForEvents failed in vecnorm: %+v \n", err)
 		}
 		WaitAndUpdateDataSliceEvents(event, glist, false)
-		return
 	}
 
-	go WaitAndUpdateDataSliceEvents(event, glist, true)
-
+	return
 }
